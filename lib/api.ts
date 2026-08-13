5983ce4f6a3954781bcd28e562ca1c8ec88813b8
VikiAinunnikmah <vikiainunnikmah273@gmail.com>
2026-08-13 21:38:59 +0700

    fix: update API_URL to new live Render backend https://phishguard-api-hrkm.onrender.com

--- lib/api.ts
+++ lib/api.ts
@@ -1,2 +1,2 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://phishguard-api-01v8.onrender.com";
+  process.env.NEXT_PUBLIC_API_URL || "https://phishguard-api-hrkm.onrender.com";
